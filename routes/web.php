f495ec48d487328de7319690ec7949568cf7db00
Dumpling <841845827@qq.com>
2019-02-04 16:00:11 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Auth::routes();
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
