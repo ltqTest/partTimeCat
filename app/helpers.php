1da6ba7cc1b58c5b7c1c810a6635d59b047894b4
Dumpling <841845827@qq.com>
2019-02-05 11:28:34 +0800

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -5,5 +5,11 @@
 // 将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
 function route_class()
 {
-return str_replace('.', '-', Route::currentRouteName());
+    return str_replace('.', '-', Route::currentRouteName());
+}
+
+// 如果 $condition 不为 True 即会返回字符串 `active`
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ Route::get('/', 'PagesController@root')->name('root');
 // 用户认证
 Auth::routes();
 
-
+// 用户
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 
-
+// 话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
