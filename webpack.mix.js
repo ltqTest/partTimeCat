34285bada1f2ee9444ceb15b3b6a960d08d4f0ad
Dumpling <841845827@qq.com>
2019-02-05 13:23:59 +0800

    WYSIWYG 编辑器

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css').version();
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .version()
+    .copyDirectory('resources/editor/js', 'public/js')
+    .copyDirectory('resources/editor/css', 'public/css');
