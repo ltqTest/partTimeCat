962c0ca794a3126aacde73f5e8a6edaf404c11e4
Dumpling <841845827@qq.com>
2019-02-19 17:09:40 +0800

    api/user

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ $api->version(
     'v1',
     [
         'namespace' => 'App\Http\Controllers\Api',
+        'middleware' => 'serializer:array',
     ],
     function ($api) {
 
@@ -55,6 +56,27 @@ $api->version(
                 // 删除token
                 $api->delete('authorizations/current', 'AuthorizationsController@destroyToken')
                     ->name('api.authorizations.destroy');
+            }
+        );
+
+        $api->group(
+            [
+                'middleware' => 'api.throttle',
+                'limit' => config('api.rate_limits.access.limit'),
+                'expires' => config('api.rate_limits.access.expires'),
+            ],
+            function ($api) {
+                // 游客可以访问的接口
+
+                // 需要 token 验证的接口
+                $api->group(
+                    ['middleware' => 'api.auth'],
+                    function ($api) {
+                        // 当前登录用户信息
+                        $api->get('user', 'UsersController@me')
+                            ->name('api.user.show');
+                    }
+                );
             }
         );
     }
