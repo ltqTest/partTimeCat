f6a7a2dc98cec3e383467addcf361fe9b5c81fe9
Dumpling <841845827@qq.com>
2019-02-19 12:55:23 +0800

    user can register using phone

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,9 +21,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'phone',
         'email',
         'password',
-        'introduction ',
+        'introduction',
         'avatar',
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -28,5 +28,8 @@ $api->version(
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
     }
 );
