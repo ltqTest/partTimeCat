c7fce9a0b0cdc914d99dfe2d3259274afe370049
Dumpling <841845827@qq.com>
2019-02-19 21:00:45 +0800

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -88,6 +88,9 @@ $api->version(
                         // 编辑话题
                         $api->patch('topics/{topic}', 'TopicsController@update')
                             ->name('api.topics.update');
+                        // 删除话题
+                        $api->delete('topics/{topic}', 'TopicsController@destroy')
+                            ->name('api.topics.destroy');
                     }
                 );
             }
