dcfc2be29dac461023618dc60eaa52f094346805
Dumpling <841845827@qq.com>
2019-02-21 22:55:22 +0800

    weapp login

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,8 @@ class User extends Authenticatable implements JWTSubject
         'weixin_openid',
         'weixin_unionid',
         'registration_id',
+        'weixin_session_key',
+        'weapp_openid',
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -50,6 +50,9 @@ $api->version(
                 // 登录
                 $api->post('authorizations', 'AuthorizationsController@store')
                     ->name('api.authorizations.store');
+                // 小程序登录
+                $api->post('weapp/authorizations', 'AuthorizationsController@weappStore')
+                    ->name('api.weapp.authorizations.store');
                 // 刷新token
                 $api->put('authorizations/current', 'AuthorizationsController@updateToken')
                     ->name('api.authorizations.update');
