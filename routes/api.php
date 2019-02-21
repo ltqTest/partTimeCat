da0081993be0ffafc401b49dac69d532b5520b75
Dumpling <841845827@qq.com>
2019-02-21 13:48:28 +0800

    notifications read

--- routes/api.php
+++ routes/api.php
@@ -119,6 +119,9 @@ $api->version(
                         // 未读通知统计
                         $api->get('user/notifications/stats', 'NotificationsController@stats')
                             ->name('api.user.notifications.stats');
+                        // 标记消息通知为已读
+                        $api->patch('user/read/notifications', 'NotificationsController@read')
+                            ->name('api.user.notifications.read');
                     }
                 );
             }
